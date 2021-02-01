ccc7e63639e88ce8c88bf834d41c0121879e15e9
LTTTTTE <eonju.dev@gmail.com>
2021-02-01 18:04:21 +0900

    feat: 오류확인

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+        InputNumberCheck inputNumberCheck = new InputNumberCheck();
+        StrikeBallCheck strikeBallCheck = new StrikeBallCheck();
 
+        int randomNumber = randomNumberGenerator.generator();
+
+        while (true) {
+            String inputNumber = scan.next();
+            if (Integer.parseInt(inputNumber) == randomNumber) {
+                System.out.println("You win!");
+                break;
+            }
+            if (inputNumberCheck.numberCheck(inputNumber)) {
+                strikeBallCheck.printResult(randomNumber, inputNumber);
+            }
+        }
     }
 }
